202c6ea653a22125e439ea330a0542cc09cdf9b8
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-13 00:43:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(sentence)
-  array = []
-  array << sentence.split
-  array.each { |word| return word.reverse }
+  sentence.split.each.reverse
+  # array.each { |word| return word.reverse }
 end
